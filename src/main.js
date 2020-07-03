f28c5e3530accd481e7ebd74f0abee8e5a40c3b7
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-07-03 16:43:06 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 
