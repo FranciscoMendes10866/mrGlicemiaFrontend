6ed88e311011b092b3bef08c9b1c99322dc0eb1e
Francisco Mendes <franciscomendes@Franciscos-MBP.Home>
2020-07-05 20:48:12 +0100

    dynamic tabs working

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -16,25 +16,57 @@
         <!-- Tabs -->
         <div class="flex flex-wrap justify-center mb-10">
           <button
-            class="py-4 px-4 bg-purple-800 text-white font-bold tracking-wide rounded hover:shadow-lg"
+            @click.prevent="selectTab(1)"
+            :class="{ 'bg-purple-800 text-white': currentTab == 1 }"
+            class="py-4 px-4 font-bold tracking-wide rounded hover:shadow-lg"
           >Mobile</button>
           <button
-            class="ml-6 py-4 px-4 bg-purple-800 text-white font-bold tracking-wide rounded hover:shadow-lg"
+            @click.prevent="selectTab(2)"
+            :class="{ 'bg-purple-800 text-white': currentTab == 2 }"
+            class="py-4 px-4 font-bold tracking-wide rounded hover:shadow-lg ml-4"
           >Secure</button>
           <button
-            class="ml-6 py-4 px-4 bg-purple-800 text-white font-bold tracking-wide rounded hover:shadow-lg"
+            @click.prevent="selectTab(3)"
+            :class="{ 'bg-purple-800 text-white': currentTab == 3 }"
+            class="py-4 px-4 font-bold tracking-wide rounded hover:shadow-lg ml-4"
           >Data</button>
           <button
-            class="ml-6 py-4 px-4 bg-purple-800 text-white font-bold tracking-wide rounded hover:shadow-lg"
+            @click.prevent="selectTab(4)"
+            :class="{ 'bg-purple-800 text-white': currentTab == 4 }"
+            class="py-4 px-4 font-bold tracking-wide rounded hover:shadow-lg ml-4"
           >More</button>
         </div>
         <!-- Cards -->
+        <!-- Card Mobile -->
         <div
+          v-if="currentTab == 1"
           class="flex flex-wrap justify-around md:justify-start lg:justify-start xl:justify-evenly"
         >
           <div
             class="max-w-sm rounded shadow-md md:shadow lg:shadow-md xl:shadow-lg mt-8 md:mt-6 lg:mt-6 xl:mt-0 lg:ml-8 animate__animated animate__bounceInUp"
             v-for="item in mobile"
+            :key="item.id"
+          >
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">
+                <i :class="item.icon">
+                  <span class="font-black text-black text-3xl tracking-wide">&nbsp;{{ item.title }}</span>
+                </i>
+              </div>
+              <p
+                class="text-gray-700 text-base mt-4 leading-loose tracking-wide mb-4"
+              >{{ item.content }}</p>
+            </div>
+          </div>
+        </div>
+        <!-- Card Secure -->
+        <div
+          v-if="currentTab == 2"
+          class="flex flex-wrap justify-around md:justify-start lg:justify-start xl:justify-evenly"
+        >
+          <div
+            class="max-w-sm rounded shadow-md md:shadow lg:shadow-md xl:shadow-lg mt-8 md:mt-6 lg:mt-6 xl:mt-0 lg:ml-8 animate__animated animate__bounceInUp"
+            v-for="item in secure"
             :key="item.id"
           >
             <div class="px-6 py-4">
@@ -58,29 +90,46 @@
 export default {
   data: () => {
     return {
+      currentTab: 1,
       mobile: [
         {
           id: 1,
-          tab: 1,
           icon: 'bx bx-mobile-alt text-3xl text-indigo-700',
           title: 'PWA',
           content: 'You can have access to the platform in any device. You can use your browser or you can install the app in your system.'
         },
         {
           id: 2,
-          tab: 1,
           icon: 'bx bx-wifi-off text-3xl text-indigo-700',
           title: 'Offline',
           content: 'You can access all your content that was already viewed by you without having any internet connection.'
         },
         {
           id: 3,
-          tab: 1,
           icon: 'bx bx-fast-forward text-3xl text-indigo-700',
           title: 'Fast',
           content: 'You can access all your content that was already viewed by you without having any internet connection.'
         }
+      ],
+      secure: [
+        {
+          id: 1,
+          icon: 'bx bx-mobile-alt text-3xl text-indigo-700',
+          title: 'Asdads',
+          content: 'You can have access to the platform in any device. You can use your browser or you can install the app in your system.'
+        },
+        {
+          id: 2,
+          icon: 'bx bx-wifi-off text-3xl text-indigo-700',
+          title: 'asdadada',
+          content: 'You can access all your content that was already viewed by you without having any internet connection.'
+        }
       ]
+    }
+  },
+  methods: {
+    selectTab (selectedTab) {
+      this.currentTab = selectedTab
     }
   }
 }
